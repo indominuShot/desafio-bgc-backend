28c29292d63e5c32c2205f5450feb28374ac9fb1
indominuShot <luang193@gmail.com>
2021-03-21 17:57:21 -0300

    Estrutura de email adicionada

---
+++ src/Utils/emailStructure.js
@@ -0,0 +1,15 @@
+const emailStructure = `
+<div>
+    <header>
+        <h2 style="text-align: center; color: #333">
+            Maravilha! Sua compra de 8 produtos foi confirmada.
+        </h2>
+
+        <h2 style="color: #8c8c8c; text-align: center;">Nós da Minion Shop agradecemos sua compra! Esperamos vê-lo(a) em breve.</h2>
+    </header>
+
+    <img style="width: 100%" src="https://a-static.mlcdn.com.br/574x431/painel-de-festa-minions-04-colormyhome/colormyhome/10605/cd65c9ed9339df95eee360d73f4a9bb4.jpg" alt="minions-background">
+</div>
+`;
+
+export default emailStructure;
