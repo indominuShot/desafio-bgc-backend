bfbe0d441f9bc1bb1e7c5fef9a259b44c0c6b04a
indominuShot <luang193@gmail.com>
2021-03-21 17:57:09 -0300

    Lambida de venda adicionada

---
+++ src/lambdas/Sale/create.js
@@ -0,0 +1,42 @@
+import { v1 } from 'uuid';
+import nodemailer from 'nodemailer';
+
+import dynamoConfig from '../../services/dynamoDB';
+import getDate from '../../Utils/getDate';
+import emailStructure from '../../Utils/emailStructure';
+
+export async function index(event) {
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: 'Sales',
+    Item: {
+      id: v1(),
+      ...data,
+      createdAt: getDate(),
+      updatedAt: getDate(),
+    },
+  };
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: `$${process.env.EMAIL_SECRET}`,
+    },
+  });
+
+  await transporter.sendMail({
+    from: `Shop Minions <${process.env.EMAIL_USER}>`, // sender address
+    to: 'luang193@gmail.com', // list of receivers
+    subject: 'Compra confirmada', // Subject line
+    html: emailStructure,
+  });
+
+  const response = await dynamoConfig.put(params);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(response),
+  };
+}
